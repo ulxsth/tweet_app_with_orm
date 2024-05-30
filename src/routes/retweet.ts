1db80188e34f2de07a3fada4691a5227e556cae3
ulxsth <ulxsth@gmail.com>
2024-05-30 20:28:33 +0900

    upd: /posts/:postId/retweets ルーターを定義

--- src/routes/retweet.ts
+++ src/routes/retweet.ts
@@ -1,3 +1,17 @@
+import { ensureAuthUser } from "@/middlewares/authentication";
+import { createRetweet } from "@/models/retweet";
 import express from "express";
 
 export const retweetRouter = express.Router();
+
+retweetRouter.post("/:postId/retweets", ensureAuthUser, async (req, res, next) => {
+  const {postId} = req.params;
+  const currentUserId = req.authentication?.currentUserId;
+  if (currentUserId === undefined) {
+    // `ensureAuthUser` enforces `currentUserId` is not undefined.
+    // This must not happen.
+    return next(new Error("Invalid error: currentUserId is undefined."));
+  }
+  await createRetweet({userId: currentUserId, postId: Number(postId)});
+  res.redirect(`/posts/${postId}`);
+})
